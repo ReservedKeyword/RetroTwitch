40dd3d6778b4e75d9f1660dbf13160ccfe5b7247
ReservedKeyword <87116177+ReservedKeyword@users.noreply.github.com>
2026-04-03 16:11:37 -0600

    update util logging

--- bun-companion/src/utils.ts
+++ bun-companion/src/utils.ts
@@ -1,10 +1,7 @@
 import { EOL } from "node:os";
-import { logger as baseLogger } from "./logger";
-
-const logger = baseLogger.getSubLogger({ name: "Utility" });
 
 export async function exitWithPause(exitCode: number): Promise<never> {
-  logger.info(`${EOL}Press Enter to exit...`);
+  console.log(`${EOL}Press Enter to exit...`);
 
   for await (const _ of console) {
     break;
